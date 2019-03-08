3f74ce909f6273792f411c9e50e543a31900a6dd
Furkan <Furkan@DESKTOP-RFSKVLF>
2019-03-08 18:29:28 +0300

    UnitOfWork Added

---
+++ BLL/BaseRepository.cs
@@ -0,0 +1,84 @@
+﻿using DAL;
+using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace BLL
+{
+    public class BaseRepository<T> where T : class
+    {
+        private DietContext _db = new DietContext();
+
+        public BaseRepository(DietContext db)
+        {
+            _db = db;
+        }
+
+        public List<T> GetAll()
+        {
+            return _db.Set<T>().ToList();
+        }
+
+        public T GetOne(int id)
+        {
+            return _db.Set<T>().Find(id);
+        }
+
+        public List<T> Search(Func<T, bool> querry)
+        {
+            return _db.Set<T>().Where(querry).ToList();
+        }
+
+        public IQueryable<T> Queryable()
+        {
+            return _db.Set<T>().AsQueryable();
+        }
+
+        public bool Add(T record)
+        {
+            try
+            {
+                _db.Set<T>().Add(record);
+                return true;
+            }
+            catch (DbEntityValidationException ex)
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                T willBeDeleted = GetOne(id);
+                _db.Set<T>().Remove(willBeDeleted);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(IEntity willBeUpdated)
+        {
+            try
+            {
+                T old = GetOne(willBeUpdated.Id);
+                _db.Entry(old).CurrentValues.SetValues(willBeUpdated);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BLL/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using DAL;
+using Entity;
+using System;
+
+namespace BLL
+{
+    public class UnitOfWork
+    {
+        public DietContext db;
+
+        public UnitOfWork()
+        {
+            object x = "";
+            if (db == null)
+            {
+                lock (x)
+                {
+                    if (db == null)
+                        db = new DietContext();
+                }
+            }
+
+            Articles = new BaseRepository<Article>(db);
+            DietLists = new BaseRepository<DietList>(db);
+            HealthInfos = new BaseRepository<HealthInfo>(db);
+            HealthInfoResults = new BaseRepository<HealthInfoResult>(db);
+            Images = new BaseRepository<Image>(db);
+            OnlineDietForms = new BaseRepository<OnlineDietForm>(db);
+            Products = new BaseRepository<Product>(db);
+            ProductTypes = new BaseRepository<ProductType>(db);
+            ProductConsumptions = new BaseRepository<ProductConsumption>(db);
+        }
+
+        public static DietContext Create()
+        {
+            return new DietContext();
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public BaseRepository<Article> Articles;
+        public BaseRepository<DietList> DietLists;
+        public BaseRepository<HealthInfo> HealthInfos;
+        public BaseRepository<HealthInfoResult> HealthInfoResults;
+        public BaseRepository<Image> Images;
+        public BaseRepository<OnlineDietForm> OnlineDietForms;
+        public BaseRepository<Product> Products;
+        public BaseRepository<ProductType> ProductTypes;
+        public BaseRepository<ProductConsumption> ProductConsumptions;
+
+    }
+}
